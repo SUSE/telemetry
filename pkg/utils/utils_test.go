1be2e2c710f2ff5aca58ebc0a7f7fae7ee52ef8e
Gabriel Bueno <gabriel.bueno@suse.com>
2024-07-09 16:55:40 -0300

    adds unit test for HumanReadableSize

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -25,3 +25,24 @@ func TestCompressDecompressGZIP(t *testing.T) {
 		t.Fatalf("Decompressed data does not match original data:\n Expected: %v, Got: %v", mockData, decompressedData)
 	}
 }
+
+// TestHumanReadableSize tests HumanReadableSize function
+func TestHumanReadableSize(t *testing.T) {
+	tests := []struct {
+		mockData []byte
+		expected string
+	}{
+		{make([]byte, 500), "500 B"},
+		{make([]byte, 1024), "1.0 KB"},
+		{make([]byte, 1536), "1.5 KB"},
+		{make([]byte, 1048576), "1.0 MB"},
+		{make([]byte, 1073741824), "1.0 GB"},
+	}
+
+	for _, test := range tests {
+		result := HumanReadableSize(test.mockData)
+		if result != test.expected {
+			t.Errorf("Error generating human readable size. Generated: %s; Expected %s", result, test.expected)
+		}
+	}
+}
