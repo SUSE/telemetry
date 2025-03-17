1fa9e3468627efc113a438c2d8748bac73a43e82
Fergal Mc Carthy <fmccarthy@suse.com>
2025-03-17 09:20:47 -0400

    Add checksum and structure validation support (#58)
    Update data item, bundle and report handling to support generating
    MD5 checksums of their content, and handle the associated error
    return values.
    
    Add or update appropriate validation directives to data item, bundle
    and report structure definitions.
    
    Removed checksum fields from bundle and report tables in the data
    store as they can only be generated once the appropriate bundle or
    report is being generated as part of submission.
    
    Also update dependency versions to the latest versions available for
    go 1.21.
    
    Ensure Makefile.golang defines an appropriate coverage file value
    if invoked directly.

--- cmd/clientds/go.mod
+++ cmd/clientds/go.mod
@@ -7,9 +7,17 @@ replace github.com/SUSE/telemetry => ../../../telemetry
 require github.com/SUSE/telemetry v0.0.0-00010101000000-000000000000
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-	github.com/xyproto/randomstring v1.0.5 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/lib/reports.go
+++ pkg/lib/reports.go
@@ -2,20 +2,85 @@ package telemetrylib
 
 import (
 	"database/sql"
+	"fmt"
 	"log/slog"
 	"strings"
 
 	"github.com/SUSE/telemetry/pkg/types"
+	"github.com/SUSE/telemetry/pkg/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 )
 
 type TelemetryReport struct {
+	// NOTE: omitempty option used in json tags to support generating test scenarios
 	Header           TelemetryReportHeader `json:"header" validate:"required"`
-	TelemetryBundles []TelemetryBundle     `json:"telemetryBundles" validate:"required,gt=0,dive"`
-	Footer           TelemetryReportFooter `json:"footer" validate:"required"`
+	TelemetryBundles []TelemetryBundle     `json:"telemetryBundles,omitempty" validate:"required,gt=0,dive"`
+	Footer           TelemetryReportFooter `json:"footer,omitempty" validate:"omitempty"`
 }
 
-func NewTelemetryReport(clientId string, tags types.Tags) *TelemetryReport {
+func (tr *TelemetryReport) UpdateChecksum() (err error) {
+	tr.Footer.Checksum, err = utils.GetMd5Hash(tr.TelemetryBundles)
+	return
+}
+
+func (tr *TelemetryReport) Validate() (err error) {
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+
+	err = validate.Struct(tr)
+	if err != nil {
+		slog.Debug(
+			"report struct validation failed",
+			slog.String("err", err.Error()),
+		)
+		err = fmt.Errorf("report struct validation check failed: %w", err)
+	}
+
+	return
+}
+
+func (tr *TelemetryReport) VerifyChecksum() (err error) {
+	err = tr.verifyBundleChecksums()
+	if err != nil {
+		return err
+	}
+
+	// report checksums optional, so skip if not specified
+	if tr.Footer.Checksum == "" {
+		return
+	}
+
+	checksum, err := utils.GetMd5Hash(&tr.TelemetryBundles)
+	if err != nil {
+		err = fmt.Errorf("failed to generate report checksum: %w", err)
+	}
+
+	if checksum != tr.Footer.Checksum {
+		err = fmt.Errorf(
+			"failed to validate report checksum: %q != %q",
+			checksum,
+			tr.Footer.Checksum,
+		)
+	}
+	return
+}
+
+func (tr *TelemetryReport) verifyBundleChecksums() (err error) {
+	for i, bundle := range tr.TelemetryBundles {
+		err = bundle.VerifyChecksum()
+		if err != nil {
+			return fmt.Errorf(
+				"failed to verify checksum for report bundle %d: %w",
+				i,
+				err,
+			)
+		}
+	}
+	return
+}
+
+func NewTelemetryReport(clientId string, tags types.Tags) (*TelemetryReport, error) {
 	tr := new(TelemetryReport)
 
 	// fill in header fields
@@ -26,21 +91,25 @@ func NewTelemetryReport(clientId string, tags types.Tags) *TelemetryReport {
 		tr.Header.ReportAnnotations = append(tr.Header.ReportAnnotations, string(a))
 	}
 
-	// fill in footer
-	tr.Footer.Checksum = "rchecksum" // TODO
+	// update the checksum
+	if err := tr.UpdateChecksum(); err != nil {
+		return nil, err
+	}
 
-	return tr
+	return tr, nil
 }
 
 type TelemetryReportHeader struct {
-	ReportId          string   `json:"reportId" validate:"required"`
+	// NOTE: omitempty option used in json tags to support generating test scenarios
+	ReportId          string   `json:"reportId,omitempty" validate:"required,uuid4"`
 	ReportTimeStamp   string   `json:"reportTimeStamp" validate:"required"`
-	ReportClientId    string   `json:"reportClientId" validate:"required"`
-	ReportAnnotations []string `json:"reportAnnotations"`
+	ReportClientId    string   `json:"reportClientId,omitempty" validate:"required,uuid4"`
+	ReportAnnotations []string `json:"reportAnnotations,omitempty"`
 }
 
 type TelemetryReportFooter struct {
-	Checksum string `json:"checksum" validate:"required"`
+	// NOTE: omitempty option used in json tags to support generating test scenarios
+	Checksum string `json:"checksum,omitempty" validate:"omitempty,md5"`
 }
 
 // Database
@@ -59,18 +128,20 @@ type TelemetryReportRow struct {
 	ReportTimestamp   string
 	ReportClientId    string
 	ReportAnnotations string
-	ReportChecksum    string
 }
 
 func NewTelemetryReportRow(clientId string, tags types.Tags) (*TelemetryReportRow, error) {
 
-	report := NewTelemetryReport(clientId, tags)
+	report, err := NewTelemetryReport(clientId, tags)
+	if err != nil {
+		return nil, err
+	}
+
 	reportRow := TelemetryReportRow{}
 	reportRow.ReportId = report.Header.ReportId
 	reportRow.ReportTimestamp = report.Header.ReportTimeStamp
 	reportRow.ReportClientId = report.Header.ReportClientId
 	reportRow.ReportAnnotations = strings.Join(report.Header.ReportAnnotations, ",")
-	reportRow.ReportChecksum = report.Footer.Checksum
 
 	return &reportRow, nil
 
@@ -93,8 +164,8 @@ func (r *TelemetryReportRow) Exists(db *sql.DB) bool {
 
 func (r *TelemetryReportRow) Insert(db *sql.DB, bundleIDs []int64) (reportId string, err error) {
 	res, err := db.Exec(
-		`INSERT INTO Reports(ReportId, ReportTimestamp, ReportClientId, ReportAnnotations, ReportChecksum) VALUES(?, ?, ?, ?, ?)`,
-		r.ReportId, r.ReportTimestamp, r.ReportClientId, r.ReportAnnotations, r.ReportChecksum,
+		`INSERT INTO Reports(ReportId, ReportTimestamp, ReportClientId, ReportAnnotations) VALUES(?, ?, ?, ?)`,
+		r.ReportId, r.ReportTimestamp, r.ReportClientId, r.ReportAnnotations,
 	)
 	if err != nil {
 		slog.Error(
